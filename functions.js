0cfa49424b07b73d03d7ff032376d08c131b9b11
9AndrewsEsteban8 <andrewsesteban7@gmail.com>
2023-07-07 18:31:30 -0500

    third commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+// Obtener el elemento h1 por su clase
+var tituloCalculadora = document.querySelector('.calculadora_tittle');
+
+// Agregar un evento al hacer clic en el título
+tituloCalculadora.addEventListener('click', function() {
+  // Tu código de manejo de evento aquí
+console.log('Se hizo clic en el título de la calculadora');
+});
